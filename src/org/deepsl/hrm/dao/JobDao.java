cb8ea90cb6269d6c8715b5cc7c6f1a6fb9e45337
93814690 <93814690@qq.com>
2017-12-25 00:03:03 +0800

    job更新

--- src/org/deepsl/hrm/dao/JobDao.java
+++ src/org/deepsl/hrm/dao/JobDao.java
@@ -11,27 +11,27 @@ import org.deepsl.hrm.domain.Job;
 import static org.deepsl.hrm.util.common.HrmConstants.JOBTABLE;
 
 
-/**   
- * @Description:  
- * @version V1.0   
+/**
+ * @version V1.0
+ * @Description:
  */
 public interface JobDao {
 
- 	Job selectById(int id);
-	
- 	List<Job> selectAllJob();
+    Job selectById(int id);
 
-	// 动态查询
- 	List<Job> selectByPage(Map<String, Object> params);
-	
- 	Integer count(Map<String, Object> params);
-	
-	// 根据id删除部门
- 	void deleteById(Integer id);
-	
-	// 动态插入部门
- 	void save(Job job);
-	
-	// 动态修改用户
- 	void update(Job job);
+    List<Job> selectAllJob();
+
+    // 动态查询
+    List<Job> selectByPage(Map<String, Object> params);
+
+    Integer count(Map<String, Object> params);
+
+    // 根据id删除部门
+    void deleteById(Integer id);
+
+    // 动态插入部门
+    void save(Job job);
+
+    // 动态修改用户
+    void update(Job job);
 }
