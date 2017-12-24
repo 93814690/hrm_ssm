c2b936a83bbe0c7fb051e208ae1154260bed7d90
lidaoyi <869695227@qq.com>
2017-12-24 14:59:17 +0800

    登录页面

--- src/org/deepsl/hrm/controller/FormController.java
+++ src/org/deepsl/hrm/controller/FormController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -16,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
-/**   
+/**
  * @Description:  
  * @date 2015年8月13日 下午8:30:37 
  * @version V1.0   
@@ -34,6 +37,33 @@ public class FormController{
 	 public String loginForm(@PathVariable String formName){
 		// 动态跳转页面
 		return formName;
+	}
+
+	@RequestMapping(value="main")
+	public String mainForm(HttpServletRequest request){
+		// 动态跳转页面
+		String ctx = request.getContextPath();
+		HttpSession session = request.getSession();
+		session.setAttribute("ctx",ctx);
+		return "main";
+	}
+
+	@RequestMapping(value="left")
+	public String leftForm(){
+		// 动态跳转页面
+		return "left";
+	}
+
+	@RequestMapping(value="right")
+	public String rightForm(){
+		// 动态跳转页面
+		return "right";
+	}
+
+	@RequestMapping(value="top")
+	public String topForm(){
+		// 动态跳转页面
+		return "top";
 	}
 
 }
