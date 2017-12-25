738439485e9f46075aa7debd7bf10a870585b809
lidaoyi <869695227@qq.com>
2017-12-25 17:40:44 +0800

    service文件提交

--- src/org/deepsl/hrm/service/impl/HrmServiceImpl.java
+++ src/org/deepsl/hrm/service/impl/HrmServiceImpl.java
@@ -1,20 +1,6 @@
 package org.deepsl.hrm.service.impl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.deepsl.hrm.dao.DeptDao;
-import org.deepsl.hrm.dao.DocumentDao;
-import org.deepsl.hrm.dao.EmployeeDao;
-import org.deepsl.hrm.dao.JobDao;
-import org.deepsl.hrm.dao.NoticeDao;
 import org.deepsl.hrm.dao.UserDao;
-import org.deepsl.hrm.domain.Dept;
-import org.deepsl.hrm.domain.Document;
-import org.deepsl.hrm.domain.Employee;
-import org.deepsl.hrm.domain.Job;
-import org.deepsl.hrm.domain.Notice;
 import org.deepsl.hrm.domain.User;
 import org.deepsl.hrm.service.HrmService;
 import org.deepsl.hrm.util.tag.PageModel;
@@ -24,105 +10,134 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-/**   
- * @Description: 人事管理系统服务层接口实现类 
- * @version V1.0   
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @version V1.0
+ * @Description: 人事管理系统服务层接口实现类
  */
-@Transactional(propagation=Propagation.REQUIRED,isolation=Isolation.DEFAULT)
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT)
 @Service("hrmService")
-public class HrmServiceImpl implements HrmService{
+public class HrmServiceImpl implements HrmService {
 
-	/**
-	 * 自动注入持久层Dao对象
-	 * */
-	@Autowired
-	private UserDao userDao;
- 
-	
-	/*****************用户服务接口实现*************************************/
-	/**
-	 * HrmServiceImpl接口login方法实现
-	 *  @see { HrmService }
-	 * */
-	@Transactional(readOnly=true)
-	@Override
-	public User login(String loginname, String password) {
+    /**
+     * 自动注入持久层Dao对象
+     */
+    @Autowired
+    private UserDao userDao;
+
+
+    /*****************用户服务接口实现*************************************/
+    /**
+     * HrmServiceImpl接口login方法实现
+     *
+     * @see { HrmService }
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public User login(String loginname, String password) {
 //		System.out.println("HrmServiceImpl login -- >>");
-		HashMap<String, String> hashMap= new HashMap<>();
-		hashMap.put("loginname",  loginname );
-		hashMap.put("password",  password );
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("loginname", loginname);
+        hashMap.put("password", password);
 
-		
-		return userDao.selectByLoginnameAndPassword( hashMap );
-	}
 
-	/**
-	 * HrmServiceImpl接口findUser方法实现
-	 * @see { HrmService }
-	 * */
-	@Transactional(readOnly=true)
-	@Override
-	public List<User> findUser(User user,PageModel pageModel) {
- 
-		List<User> users = null;
-		 
-		return users;
-	}
-	
-	/**
-	 * HrmServiceImpl接口findUserById方法实现
-	 * @see { HrmService }
-	 * */
-	@Transactional(readOnly=true)
-	@Override
-	public User findUserById(Integer id) {
-		return userDao.selectById(id);
-	}
-	
-	/**
-	 * HrmServiceImpl接口removeUserById方法实现
-	 * @see { HrmService }
-	 * */
-	@Override
-	public void removeUserById(Integer id) {
- 		
-	}
-	
-	/**
-	 * HrmServiceImpl接口addUser方法实现
-	 * @see { HrmService }
-	 * */
-	@Override
-	public void modifyUser(User user) {
-		userDao.update(user);
-		
-	}
-	
-	/**
-	 * HrmServiceImpl接口modifyUser方法实现
-	 * @see { HrmService }
-	 * */
-	@Override
-	public void addUser(User user) {
- 		
-	}
-	
-	/*****************部门服务接口实现*************************************/
- 
-	/*****************员工服务接口实现*************************************/
- 
-	
-	/*****************职位接口实现*************************************/
+        return userDao.getByLoginnameAndPassword(hashMap);
+    }
 
- 
-	
-	/*****************公告接口实现*************************************/
-	 
+    /**
+     * HrmServiceImpl接口findUser方法实现
+     *
+     * @see { HrmService }
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> findUser(User user, PageModel pageModel) {
 
-	/*****************文件接口实现*************************************/
+        Map<String, Object> params = new HashMap<>();
+        String username = user.getUsername();
+        Integer status = user.getStatus();
+        if (username != null && !username.isEmpty())
+            params.put("username", "%" + username + "%");
+        if (status != null)
+            params.put("status", status);
+        Integer count = userDao.count(params);
+        if (count <= 0)
+            return null;
+        pageModel.setRecordCount(count);
+        int limit = pageModel.getPageSize();
+        params.put("limit", limit);
+        int offset = pageModel.getFirstLimitParam();
+        params.put("offset", offset);
+        List<User> users = userDao.listByPage(params);
 
- 
- 
- 
-	
+        return users;
+    }
+
+    /**
+     * HrmServiceImpl接口findUserById方法实现
+     *
+     * @see { HrmService }
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public User findUserById(Integer id) {
+        return userDao.getById(id);
+    }
+
+    /**
+     * HrmServiceImpl接口removeUserById方法实现
+     *
+     * @see { HrmService }
+     */
+    @Override
+    public void removeUserById(Integer id) {
+        userDao.deleteById(id);
+    }
+
+    /**
+     * HrmServiceImpl接口addUser方法实现
+     *
+     * @see { HrmService }
+     */
+    @Override
+    @Transactional
+    public void modifyUser(User user) {
+        userDao.update(user);
+
+    }
+
+    /**
+     * HrmServiceImpl接口modifyUser方法实现
+     *
+     * @see { HrmService }
+     */
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        userDao.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void removeUserByIds(int[] ids) {
+        userDao.deleteByIds(ids);
+    }
+
+    /*****************部门服务接口实现*************************************/
+
+    /*****************员工服务接口实现*************************************/
+
+
+    /*****************职位接口实现*************************************/
+
+
+    /*****************公告接口实现*************************************/
+
+
+    /*****************文件接口实现*************************************/
+
+
 }
